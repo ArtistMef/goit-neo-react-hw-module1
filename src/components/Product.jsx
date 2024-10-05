a5d7c5ffe4d2c3e1cfb33eea7743a4e7f678130d
Ihor <Inferion69@gmail.com>
2024-10-05 15:45:43 +0300

    +

---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+// src/Product.jsx
+
+export default function Product({ name, price, imgUrl }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
+    </div>
+  );
+}
